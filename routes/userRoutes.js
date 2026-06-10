4e37916377981a0bbfc8374a304327c6d48e5fc7
Sow123web <drjsp2007@gmail.com>
2026-06-10 20:59:25 +0530

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -96,7 +96,7 @@ router.post("/login", async (req, res) => {
 
         // CHROME OTP LOGIC
 
-        if(false && req.body.browser === "Chrome") {
+        if(req.body.browser === "Chrome") {
 
             const otp =
             Math.floor(100000 + Math.random() * 900000)
